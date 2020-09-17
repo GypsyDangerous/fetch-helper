7f2442e53a7feb4259dcdea9a842e26af01e22af
David Snyder <davidgraygs4@gmail.com>
2020-09-17 17:48:46 -0400

    fixed type issue with node-fetch types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import nodeFetch from "node-fetch";
-// import {RequestInfo, RequestInit} from "@types/node-fetch"
-const fetch = async (url: string, options: any = {}): Promise<any> => {
+import nodeFetch, {RequestInit} from "node-fetch";
+
+const fetch = async (url: string, options?: RequestInit): Promise<any> => {
 	const response = await nodeFetch(url, options);
 	const contentType = response.headers.get("content-type");
 	let responseData;
