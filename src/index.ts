473828c3b276c7eba2e1f158a91907da436e38a0
David Snyder <davidgraygs4@gmail.com>
2020-09-17 18:34:23 -0400

    🚀return a clone of the response as long as the data.
    also improve content type checking

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
 import nodeFetch, {RequestInit} from "node-fetch";
 
 const fetch = async (url: string, options?: RequestInit): Promise<any> => {
-	const response = await nodeFetch(url, options);
+    const response = await nodeFetch(url, options);
+    const clone = response.clone()
 	const contentType = response.headers.get("content-type");
 	let responseData;
 	if (contentType?.includes("application/json")) {
 		responseData = await response.json();
 	} else if (contentType?.includes("text/html")) {
 		responseData = await response.text();
+    }else{
+        try{
+            responseData = await response.clone().json()
+        }catch(err){
+            responseData = await response.text()
+        }
     }
     
 	if (!response.ok || !responseData) {
 		responseData = { message: responseData?.message || response.statusText, code: responseData?.code || responseData?.status || response.status };
 	}
 
-	return responseData;
+	return {...responseData, rawResponse: clone};
 };
 
 export = fetch;
